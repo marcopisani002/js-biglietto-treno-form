67180144a4d483b4076cfcc4a4214396ad0389ca
marcopisani002 <pisanimarco34@gmail.com>
2022-10-28 18:24:23 +0200

    add navbar

--- js/main.js
+++ js/main.js
@@ -11,23 +11,19 @@ mainBtn.addEventListener("click", function () {
     console.log("Tipologia:", typeof inputEl);
 
     //stampa HTML -->NOME E COGNOME
-    const finalPriceEl = document.getElementById("printname");
-
-   
-
+    const printName = document.getElementById("printname");
+    printName.innerHTML = inputEl.value;
 
 
-    // finalPriceEl.innerHTML = "€ " + prezzoFinale.toFixed( 2 );
-    finalPriceEl.innerHTML = inputEl;
 
 
     // input KILOMETRAGGIO
 
     const KmTreno = parseInt(document.getElementById("trip-km").value);
-
     const prezzo = KmTreno * 0.21;
     console.log("km Inseriti:", KmTreno);
 
+    // input PREZZO NON SCONTATO
 
     console.log("prezzo:", prezzo);
     console.log("Tipologia:", typeof KmTreno);
@@ -42,9 +38,19 @@ mainBtn.addEventListener("click", function () {
 
 
 
-
     if (sconto != 20 && sconto != 40) {
         console.log("L'utente ha un'età compresa tra i 18 e i 65 e NON ha diritto agli sconti.");
+
+
+        //stampa HTML -->PREZZO NON SCONTATO
+
+        const finalPrice = document.getElementById("printprice");
+        finalPrice.innerHTML = `€ ${prezzo.toFixed(2)}`;
+
+        //stampa HTML -->OFFERTA
+
+        const temporaryPrice = document.getElementById("printoffer");
+        temporaryPrice.innerHTML = `NO`;
     }
 
     // sconto per under 18 = 20%
@@ -54,6 +60,17 @@ mainBtn.addEventListener("click", function () {
         // prezzo arrotondato per difetto
         const numFloor = Math.floor(prezzoSale);
         console.log("Prezzo finale scontato UNDER 18:", numFloor);
+
+        //stampa HTML -->OFFERTA
+
+        const temporaryPrice = document.getElementById("printoffer");
+        temporaryPrice.innerHTML = `SI`;
+
+        //stampa HTML -->PREZZO SCONTATO
+
+        const finalPrice = document.getElementById("printprice");
+        finalPrice.innerHTML = `€ ${prezzoSale.toFixed(2)}`;
+
     }
 
     // sconto over 65 = 40%
@@ -62,9 +79,20 @@ mainBtn.addEventListener("click", function () {
         // prezzo arrotondato per eccesso
         const numCeil = Math.ceil(prezzoSale);
         console.log("Prezzo finale scontato OVER 65:", numCeil);
+
+        //stampa HTML -->OFFERTA
+
+        const temporaryPrice = document.getElementById("printoffer");
+        temporaryPrice.innerHTML = `SI`;
+
+
+        //stampa HTML -->PREZZO SCONTATO
+
+        const finalPrice = document.getElementById("printprice");
+        finalPrice.innerHTML = `€ ${prezzoSale.toFixed(2)}`;
     }
 
-    console.log("hai cliccato qui");
+
 
 }
 )
