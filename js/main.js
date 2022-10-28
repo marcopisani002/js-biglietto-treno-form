acb1c4cb84c801163d5c48b9121b46c481b9d9fa
marcopisani002 <pisanimarco34@gmail.com>
2022-10-28 17:13:05 +0200

    add discount

--- js/main.js
+++ js/main.js
@@ -6,46 +6,47 @@ console.log("Tipologia:", typeof mainBtn);
 mainBtn.addEventListener("click", function () {
     // input NOME E COGNOME
     const inputEl = document.getElementById("complete-nocg");
-    console.log( inputEl);
+    console.log(inputEl);
     console.log("Nome inserito:", inputEl.value);
     console.log("Tipologia:", typeof inputEl);
 
     // input KILOMETRAGGIO
 
-    const KmTreno =  document.getElementById("trip-km");
+    const KmTreno =parseInt(document.getElementById("trip-km").value) ;
+    
     const prezzo = KmTreno * 0.21;
-    console.log( KmTreno);
-    console.log("km Inseriti:",KmTreno.value);
-    console.log(prezzo);
-    console.log( "prezzo:",prezzo.value);
+    console.log("km Inseriti:", KmTreno);
+    
+    
+    console.log("prezzo:", prezzo);
     console.log("Tipologia:", typeof KmTreno);
-   
+
 
     // input ETA'
 
-    const age = document.getElementById("range");
-    console.log(age);
-    console.log("età:",age);
-    console.log("Tipologia:", typeof age);
+    const sconto = document.getElementById("range").value;
+    
+    console.log("sconto:", sconto,"%");
+    console.log("Tipologia:", typeof sconto);
 
 
 
 
-    if (age > 18 && age < 65) {
+    if (sconto !=20 && sconto !=40) {
         console.log("L'utente ha un'età compresa tra i 18 e i 65 e NON ha diritto agli sconti.");
     }
 
     // sconto per under 18 = 20%
 
-    if (age < 18) {
-        const prezzoSale = prezzo * 0.8;
+    if (sconto ==20) {
+        const prezzoSale = prezzo- ((prezzo * sconto)/100);
         // prezzo arrotondato per difetto
         const numFloor = Math.floor(prezzoSale);
         console.log("Prezzo finale scontato UNDER 18:", numFloor);
     }
 
     // sconto over 65 = 40%
-    if (age > 65) {
+    if (sconto == 40) {
         const prezzoSale = prezzo * 0.6;
         // prezzo arrotondato per eccesso
         const numCeil = Math.ceil(prezzoSale);
