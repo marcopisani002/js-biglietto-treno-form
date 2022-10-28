ff085b4d532ba15d981263ff06822fff634080e4
marcopisani002 <pisanimarco34@gmail.com>
2022-10-28 17:39:57 +0200

    add inner html

--- js/main.js
+++ js/main.js
@@ -10,14 +10,25 @@ mainBtn.addEventListener("click", function () {
     console.log("Nome inserito:", inputEl.value);
     console.log("Tipologia:", typeof inputEl);
 
+    //stampa HTML -->NOME E COGNOME
+    const finalPriceEl = document.getElementById("printname");
+
+   
+
+
+
+    // finalPriceEl.innerHTML = "€ " + prezzoFinale.toFixed( 2 );
+    finalPriceEl.innerHTML = inputEl;
+
+
     // input KILOMETRAGGIO
 
-    const KmTreno =parseInt(document.getElementById("trip-km").value) ;
-    
+    const KmTreno = parseInt(document.getElementById("trip-km").value);
+
     const prezzo = KmTreno * 0.21;
     console.log("km Inseriti:", KmTreno);
-    
-    
+
+
     console.log("prezzo:", prezzo);
     console.log("Tipologia:", typeof KmTreno);
 
@@ -25,21 +36,21 @@ mainBtn.addEventListener("click", function () {
     // input ETA'
 
     const sconto = document.getElementById("range").value;
-    
-    console.log("sconto:", sconto,"%");
+
+    console.log("sconto:", sconto, "%");
     console.log("Tipologia:", typeof sconto);
 
 
 
 
-    if (sconto !=20 && sconto !=40) {
+    if (sconto != 20 && sconto != 40) {
         console.log("L'utente ha un'età compresa tra i 18 e i 65 e NON ha diritto agli sconti.");
     }
 
     // sconto per under 18 = 20%
 
-    if (sconto ==20) {
-        const prezzoSale = prezzo- ((prezzo * sconto)/100);
+    if (sconto == 20) {
+        const prezzoSale = prezzo - ((prezzo * sconto) / 100);
         // prezzo arrotondato per difetto
         const numFloor = Math.floor(prezzoSale);
         console.log("Prezzo finale scontato UNDER 18:", numFloor);
